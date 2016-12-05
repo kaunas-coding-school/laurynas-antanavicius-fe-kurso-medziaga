3ef680dbfb31e6da46b52597c32c6d794968e73f
Laurynas <laurynas@experimental-foundation.com>
2016-12-06 01:18:48 +0200

    initial commit

---
+++ examples/lecture-1/404-page/scripts.js
@@ -0,0 +1,5 @@
+// Log information in the console the user click the link
+function track() {
+  document.getElementById("title").innerHTML = "4+0+4=8";
+  console.log('User clicked on home link');
+}
