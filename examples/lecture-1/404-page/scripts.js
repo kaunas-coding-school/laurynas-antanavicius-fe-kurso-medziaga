67ded9a851e476f852ba4a5c1f59490bae2fe057
Laurynas <laurynas@experimental-foundation.com>
2016-12-07 23:37:26 +0200

    updated files and slides

--- examples/lecture-1/404-page/scripts.js
+++
@@ -1,5 +0,0 @@
-// Log information in the console the user click the link
-function track() {
-  document.getElementById("title").innerHTML = "4+0+4=8";
-  console.log('User clicked on home link');
-}
