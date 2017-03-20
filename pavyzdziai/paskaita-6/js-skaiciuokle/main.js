e2b92a207e8aa126f66f2920bb5383e362db3040
Laurynas <laurynas.ant@gmail.com>
2017-03-20 20:55:20 +0200

    veikiantis kalkuliatorius

--- pavyzdziai/paskaita-6/js-skaiciuokle/main.js
+++ pavyzdziai/paskaita-6/js-skaiciuokle/main.js
@@ -2,14 +2,25 @@ console.log("Hello");
 
 // 1. Pasirenkam mygtuka is html strukturos
 var numberButton = document.querySelectorAll(".js-number-button");
-console.log(numberButton);
+// console.log(numberButton);
 var screenInput = document.querySelector("#js-screen");
 // console.log(screenInput);
+var operatorInput = document.querySelectorAll(".js-operator-button");
+// console.log(operatorInput);
+var resultInput = document.querySelector("#js-result");
+
+var firstNumber, operatorValue;
 
 // 2. Sekam paspaudimo veiksmą
 for (var i = 0; i < numberButton.length; i++) {
     numberButton[i].addEventListener("click", enterNumber);
 }
+
+for (var i = 0; i < operatorInput.length; i++) {
+    operatorInput[i].addEventListener("click", saveNumber);
+}
+
+resultInput.addEventListener("click", showResult);
 
 // Pirmoji funkcija numeriu ivedimui
 function enterNumber(event) {
@@ -19,4 +30,40 @@ function enterNumber(event) {
     } else {
         screenInput.value += Number(event.target.value);
     }
+}
+
+// TODO Funkcija kuri issaugo skaiciu ir veiksma
+function saveNumber(event) {
+    firstNumber = Number(screenInput.value);
+    operatorValue = event.target.value;
+    // console.log(firstNumber, operatorValue);
+    screenInput.value = 0;
+}
+
+// TODO Funkcija kuri parodo rezultata
+function showResult() {
+    var secondNumber = Number(screenInput.value);
+    var result;
+
+    switch (operatorValue) {
+        case "X":
+            result = firstNumber * secondNumber;
+            break;
+        case "/":
+            result = firstNumber / secondNumber;
+            break;
+        case "+":
+            result = firstNumber + secondNumber;
+            break;
+        case "-":
+            result = firstNumber - secondNumber;
+            break;
+        default:
+            console.error("No operator value received");
+    }
+    screenInput.value = result;
+    // result = firstNumber * secondNumber;
+    // result = firstNumber / secondNumber;
+    // result = firstNumber + secondNumber;
+    // result = firstNumber - secondNumber;
 }
