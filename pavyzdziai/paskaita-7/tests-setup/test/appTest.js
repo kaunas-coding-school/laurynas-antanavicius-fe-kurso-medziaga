958fbd7a4ad661aa550e9c53c4dd1c2870495091
Laurynas <laurynas.ant@gmail.com>
2017-03-22 18:58:41 +0200

    final pavyzdys su testais

--- pavyzdziai/paskaita-7/tests-setup/test/appTest.js
+++ pavyzdziai/paskaita-7/tests-setup/test/appTest.js
@@ -1,7 +1,19 @@
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-        expect([1,2,3].indexOf(4)).to.be(-1);
+// describe('Array', function() {
+//   describe('#indexOf()', function() {
+//     it('should return -1 when the value is not present', function() {
+//         expect([1,2,3].indexOf(4)).to.be(-1);
+//     });
+//   });
+// });
+
+describe('App', function () {
+    describe('sum()', function() {
+        it('should return 4 if I add 2+2', function() {
+            expect(sum(2, 2)).to.be(4);
+        });
+
+        it('should return 8 if I add -2 and 10', function() {
+            expect(sum(-2, 10)).to.be(8);
+        });
     });
-  });
 });
